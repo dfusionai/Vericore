cd2955aee2168fb777b72cef361b965845f18193
mitchell <mitchell@policydock.com>
2025-04-11 17:11:48 +0200

    Added http2

--- shared/http_helper.py
+++ shared/http_helper.py
@@ -5,11 +5,11 @@ REQUEST_TIMEOUT_SECONDS = 60
 headers = { "User-Agent": "Mozilla/5.0" }  # Mimic a real browser
 
 async def send_get_request(endpoint: str, headers: dict = None):
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(http2=True) as client:
         response = await client.get(endpoint, timeout=REQUEST_TIMEOUT_SECONDS, headers=headers )
         return response
 
 async def send_post_request(endpoint: str, json_data, headers: dict = None):
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(http2=True) as client:
         response = await client.post(endpoint, json=json_data, timeout=REQUEST_TIMEOUT_SECONDS, headers=headers)
         return response
