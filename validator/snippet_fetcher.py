86d5ae691901c2a13c381cf8b06974bad9f065ba
mitchell <mitchell@policydock.com>
2025-04-14 13:55:13 +0200

    Optimised snippet fetcher.

--- validator/snippet_fetcher.py
+++ validator/snippet_fetcher.py
@@ -1,7 +1,7 @@
 import datetime
-
-import requests
-
+import asyncio
+# import requests
+import httpx
 from bs4 import BeautifulSoup
 
 
@@ -9,8 +9,33 @@ import bittensor as bt
 
 from shared.http_helper import send_get_request
 
+REQUEST_TIMEOUT_SECONDS = 60
 
 class SnippetFetcher:
+
+
+    def __int__(self):
+        # Initialize a shared client once
+        self.client = httpx.AsyncClient(
+            http2=True,
+            headers={ "User-Agent": "Mozilla/5.0" },
+            timeout=60.0  # Adjust as needed
+        )
+
+    async def send_get_request(self, endpoint: str):
+            return await self.client.get(endpoint, timeout=REQUEST_TIMEOUT_SECONDS)
+
+    async def clean_html(self, url: str, html :str) -> str:
+        bt.logging.info(f"{ url } | Cleaning html")
+        soup = BeautifulSoup(html, "lxml")  # 5-10x faster than html.parser
+
+        bt.logging.info(f"{url} | Decomposing  html")
+
+        # Single-pass removal using CSS selectors
+        for tag in soup.select("script, iframe, ins, aside, noscript"):
+            tag.decompose()
+
+        return str(soup)
 
     async def fetch_entire_page(self, url: str) -> str:
       """
@@ -24,26 +49,20 @@ class SnippetFetcher:
 
           start_time = datetime.datetime.now()
           try:
-            response = await send_get_request(url, headers=headers) #requests.get(url, headers=headers)
+            response = await self.send_get_request(url) #requests.get(url, headers=headers)
           finally:
               bt.logging.info(f"Received response: {url} : {response.status} : {(datetime.datetime.now() - start_time).total_seconds()} seconds" )
 
-          if response.status_code == 200:
-              bt.logging.info(f"Passing to Beautiful soup for cleaning: {url}")
-
-              soup = BeautifulSoup(response.text, "html.parser")
-
-              bt.logging.info(f"Cleaning html")
-
-              # Remove common ad elements
-              for tag in soup.find_all(["script", "iframe", "ins", "aside", "noscript"]):
-                  tag.decompose()  # Remove the tag from the DOM
-
-              clean_html = str(soup)
-              return clean_html
-          else:
+          if response.status_code != 200:
               bt.logging.error(f"Failed to fetch {url} - {response.status_code}")
               return ""
+
+          response = await asyncio.to_thread(self.clean_html, response.text)
+
+          bt.logging.info(f"{url} | Fetched html | {(datetime.datetime.now() - start_time).total_seconds()} seconds")
+
+          return response
+
       except Exception as e:
           bt.logging.error(f"Failed to fetch {url} - {e}")
           return ""
