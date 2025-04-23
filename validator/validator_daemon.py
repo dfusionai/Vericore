b60992850387fcf92707d0737563628158b70c5c
mitchell <mitchell@policydock.com>
2025-04-23 19:07:55 +0200

    Updated data to send json data as validator response.

--- validator/validator_daemon.py
+++ validator/validator_daemon.py
@@ -6,12 +6,17 @@ import traceback
 import numpy as np
 import bittensor as bt
 import logging
-
+from typing import List
 from shared.debug_util import DEBUG_LOCAL
 from shared.log_data import LoggerType
 from shared.proxy_log_handler import register_proxy_log_handler
-
+from shared.store_results_handler import (
+    register_validator_results_data_handler,
+    ValidatorResultsDataHandler,
+)
 from dotenv import load_dotenv
+
+from shared.validator_results_data import ValidatorResultsData
 
 bt.logging.set_trace()
 
@@ -61,6 +66,25 @@ def setup_logging(wallet, config):
     register_proxy_log_handler(bt_logger, LoggerType.Validator, wallet)
 
 
+def send_validator_response_data(
+    store_response_handler: ValidatorResultsDataHandler,
+    vericore_responses: List[dict],
+    moving_scores: List[float],
+    weights: List[float],
+    incentives: List[float],
+):
+
+    if store_response_handler is not None:
+        bt.logging.info(f"Sending validator response data: {weights}, {incentives}")
+        validator_response_data = ValidatorResultsData()
+        validator_response_data.timestamp = time.time()
+        validator_response_data.vericore_responses = vericore_responses
+        validator_response_data.moving_scores = moving_scores
+        validator_response_data.calculated_weights = weights
+        validator_response_data.incentives = incentives
+        store_response_handler.send_json(validator_response_data)
+
+
 def aggregate_results(results_dir, moving_scores):
     """
     Scan the results directory for JSON files (each a query result), update moving_scores
@@ -72,12 +96,17 @@ def aggregate_results(results_dir, moving_scores):
         if f.endswith(".json")
     ]
     if not files:
-        return moving_scores
+        return moving_scores, None
+
+    vericore_responses = []
 
     for filepath in files:
         try:
+
             with open(filepath, "r") as f:
                 result = json.load(f)
+                vericore_responses.append(result)
+
             for res in result.get("results", []):
                 miner_uid = res.get("miner_uid")
                 final_score = res.get("final_score")
@@ -85,18 +114,19 @@ def aggregate_results(results_dir, moving_scores):
                     calculated_score = moving_scores[miner_uid] + final_score
 
                     bt.logging.info(
-                        f"Moving score for uid: {miner_uid} and final score: {final_score} with calculated scored {calculated_score}"
+                        f"DAEMON | Moving score for uid: {miner_uid} and final score: {final_score} with calculated scored {calculated_score}"
                     )
                     moving_scores[miner_uid] = calculated_score
+
         except Exception as e:
-            bt.logging.error(f"Error processing file {filepath}: {e}")
+            bt.logging.error(f"DAEMON | Error processing file {filepath}: {e}")
         finally:
             try:
                 os.remove(filepath)
-                bt.logging.info(f"Deleted processed file {filepath}")
+                bt.logging.info(f"DAEMON | Deleted processed file {filepath}")
             except Exception as e:
-                bt.logging.error(f"Error deleting file {filepath}: {e}")
-    return moving_scores
+                bt.logging.error(f"DAEMON | Error deleting file {filepath}: {e}")
+    return moving_scores, vericore_responses
 
 
 def main():
@@ -105,15 +135,20 @@ def main():
     setup_logging(wallet, config)
     results_dir = "results"
     os.makedirs(results_dir, exist_ok=True)
+    my_uid = metagraph.hotkeys.index(wallet.hotkey.ss58_address)
+
+    validator_results_data_handler = register_validator_results_data_handler(
+        my_uid, wallet
+    )
 
     tempo = subtensor.tempo(config.netuid)
-    my_uid = metagraph.hotkeys.index(wallet.hotkey.ss58_address)
-    bt.logging.info("Starting Validator Daemon loop.")
+
+    bt.logging.info("DAEMON | Starting Validator Daemon loop.")
     while True:
         try:
             last_update = subtensor.blocks_since_last_update(config.netuid, my_uid)
             bt.logging.info(
-                f"Will aggregate results: {last_update} > {tempo + 1} = {last_update > tempo + 1} "
+                f"DAEMON | Will aggregate results: {last_update} > {tempo + 1} = {last_update > tempo + 1} "
             )
             if last_update > tempo + 1:
             # if True:
@@ -122,22 +157,26 @@ def main():
 
                 # create new moving scores array in case new miners have been loaded
                 moving_scores = [INITIAL_WEIGHT] * len(metagraph.S)
-                moving_scores = aggregate_results(results_dir, moving_scores)
+                moving_scores, vericore_responses = aggregate_results(
+                    results_dir, moving_scores
+                )
+
+                bt.logging.info(f"DAEMON | Moving scores: {moving_scores}")
 
                 if all(scores == 0 for scores in moving_scores):
-                    bt.logging.warning(f"Skipped setting of weights")
+                    bt.logging.warning(f"DAEMON | Skipped setting of weights")
                     # Sleep for 5 seconds
                     time.sleep(10)
                     # Don't update weights if  all moving scores are 0 otherwise it might rate the weights equally.
                     continue
 
-                bt.logging.info(f"Moving scores: {moving_scores}")
+                bt.logging.info(f"DAEMON | Moving scores: {moving_scores}")
                 arr = np.array(moving_scores, dtype=np.float32)
-                scale   = 8.0
-                deltas  = arr.max() - arr
+                scale = 8.0
+                deltas = arr.max() - arr
                 exp_dec = np.exp(-deltas / scale)
-                weights = ((exp_dec / exp_dec.sum())*65535).tolist()
-                bt.logging.info(f"Setting weights on chain: {weights}")
+                weights = ((exp_dec / exp_dec.sum()) * 65535).tolist()
+                bt.logging.info(f"DAEMON | Setting weights on chain: {weights}")
                 subtensor.set_weights(
                     netuid=config.netuid,
                     wallet=wallet,
@@ -145,17 +184,29 @@ def main():
                     weights=weights,
                     wait_for_inclusion=True,
                 )
-                if DEBUG_LOCAL:
-                    for neuron in subtensor.neurons(netuid=config.netuid):
-                        print(f"Miner UID: {neuron.uid} INCENTIVE: {neuron.incentive} ")
+
+                incentives = [
+                    neuron.incentive
+                    for neuron in subtensor.neurons(netuid=config.netuid)
+                ]
+
+                bt.logging.info("DAEMON | Preparing to send json data")
+
+                send_validator_response_data(
+                    validator_results_data_handler,
+                    vericore_responses,
+                    moving_scores,
+                    weights,
+                    incentives,
+                )
 
             metagraph.sync()
             time.sleep(60)
         except KeyboardInterrupt:
-            bt.logging.info(f"Validator Daemon interrupted. Exiting.")
+            bt.logging.info(f"DAEMON | Validator Daemon interrupted. Exiting.")
             break
         except Exception as e:
-            bt.logging.error(f"Error in daemon loop: {e}")
+            bt.logging.error(f"DAEMON | Error in daemon loop: {e}")
             traceback.print_exc()
             time.sleep(10)
 
